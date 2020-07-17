0a5f4bc32134823d932ebe450dbc5021c7c6305e
Shawn Gluchacki <Shawn@MacBook-Pro-2.local>
2020-07-17 09:02:00 -0600

    Updates

---
+++ precache-manifest.4165bb913f3941932352cba6cb21ecbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17beb498e5d768fdb5e685db4bb4ce5d",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "8e0379f98226ca70e727",
+    "url": "/project-4-assessment/static/css/main.02d45a0c.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0379f98226ca70e727",
+    "url": "/project-4-assessment/static/js/main.af50b7bb.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
